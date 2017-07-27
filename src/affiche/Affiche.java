d7fa2841939c104f09a7cb912b0a494bfbf14a8d
post <postglpi@gmail.com>
2017-07-27 13:38:30 +0100

    premier commit

---
+++ src/affiche/Affiche.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package affiche;
+
+/**
+ *
+ * @author michelB
+ */
+public class Affiche {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        if(args.length > 0){
+            System.out.println(args[0]);
+        }else{
+            System.out.println("Pas de paramètre");
+        }
+    }
+    
+}
